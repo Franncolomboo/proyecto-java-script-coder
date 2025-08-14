187ac1fe8373467d9e24ed4e9b19072ae4d1092e
Francisco Colombo <franciscocolombounlp@gmail.com>
2025-08-14 16:23:20 -0300

    Primera actividad Js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let num = 5;
+let palabra = "Hola";
+let ok = true;
+let num2 = Number(prompt("Ingrese un número:"));
+let suma = num + num2;
+
+console.log(num);
+console.log(palabra);
+console.log(ok);
+console.log(suma);  
