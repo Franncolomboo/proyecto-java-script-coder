2be774f07fc94ea1b995bd64b27de2b9c5d390be
Francisco Colombo <franciscocolombounlp@gmail.com>
2025-08-15 10:30:16 -0300

    Primeros estilos con scss

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-let num = 5;
-let palabra = "Hola";
-let ok = true;
-let num2 = Number(prompt("Ingrese un número:"));
-let suma = num + num2;
+// let num = 5;
+// let palabra = "Hola";
+// let ok = true;
+// let num2 = Number(prompt("Ingrese un número:"));
+// let suma = num + num2;
 
-console.log(num);
-console.log(palabra);
-console.log(ok);
-console.log(suma);  
+// console.log(num);
+// console.log(palabra);
+// console.log(ok);
+// console.log(suma);  
